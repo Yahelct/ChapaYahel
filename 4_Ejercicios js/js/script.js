a3fbd9cdd30820f526ae303dcbca72f592dafabd
Yahelct <yahelchapa09@gmail.com>
2024-02-04 19:53:01 -0600

    Ejercicios js

--- 4_Ejercicios js/js/script.js
+++ 4_Ejercicios js/js/script.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Cargar el primer proyecto por defecto al cargar la página
+   
     cargarProyecto('01_Intereses/Index.html');
 
-    // Agregar manejadores de eventos a los enlaces
     const enlacesProyectos = document.querySelectorAll('nav ul li a');
     enlacesProyectos.forEach(enlace => {
         enlace.addEventListener('click', function (event) {
-            event.preventDefault(); // Evitar la recarga de la página al hacer clic en un enlace
+            event.preventDefault(); 
             const rutaProyecto = this.getAttribute('href');
             cargarProyecto(rutaProyecto);
         });
@@ -16,6 +15,5 @@ document.addEventListener('DOMContentLoaded', function () {
 function cargarProyecto(rutaProyecto) {
     const proyectoIframe = document.getElementById('proyecto-iframe');
     
-    // Agregamos un timestamp para evitar problemas de caché
     proyectoIframe.src = `${rutaProyecto}?${Date.now()}`;
 }
